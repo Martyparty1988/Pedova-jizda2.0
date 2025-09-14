db7b1750b610777321cb7f8affab3abc6991185a
Marty <rosary-vassals8m@icloud.com>
2025-09-14 17:51:49 +0200

    Update game-audio.js

--- game-audio.js
+++ game-audio.js
@@ -25,14 +25,15 @@ class GameAudio {
         const o = this.context.createOscillator();
         const g = this.context.createGain();
         o.connect(g);
-        g.connect(this.context.destination);
+g.connect(this.context.destination);
         g.gain.setValueAtTime(0.2, t);
         switch (type) {
             case 'jump': o.frequency.setValueAtTime(300, t); o.frequency.exponentialRampToValueAtTime(600, t + 0.1); break;
+            // ZMĚNA: Nový zvuk pro super skok
+            case 'super_jump': o.type = 'sine'; o.frequency.setValueAtTime(200, t); o.frequency.exponentialRampToValueAtTime(1000, t + 0.2); g.gain.setValueAtTime(0.3, t); break;
             case 'dash': o.type = 'sawtooth'; o.frequency.setValueAtTime(600, t); o.frequency.exponentialRampToValueAtTime(100, t + 0.2); break;
             case 'collision': o.type = 'square'; o.frequency.setValueAtTime(150, t); o.frequency.exponentialRampToValueAtTime(40, t + 0.4); break;
             case 'powerup': o.frequency.setValueAtTime(600, t); o.frequency.exponentialRampToValueAtTime(1200, t + 0.05); o.frequency.exponentialRampToValueAtTime(800, t + 0.2); break;
-            // ZMĚNA: Nové zvuky pro štít
             case 'powerup_shield': o.type = 'sine'; o.frequency.setValueAtTime(800, t); o.frequency.exponentialRampToValueAtTime(1600, t + 0.2); break;
             case 'shield_break': o.type = 'noise'; g.gain.setValueAtTime(0.3, t); o.frequency.setValueAtTime(1000, t); break;
         }

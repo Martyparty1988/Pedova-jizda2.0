ff8a0f122c7c0412c25e04402faada16719e3829
Marty <rosary-vassals8m@icloud.com>
2025-09-14 15:58:10 +0200

    Update game-audio.js

--- game-audio.js
+++ game-audio.js
@@ -32,6 +32,9 @@ class GameAudio {
             case 'dash': o.type = 'sawtooth'; o.frequency.setValueAtTime(600, t); o.frequency.exponentialRampToValueAtTime(100, t + 0.2); break;
             case 'collision': o.type = 'square'; o.frequency.setValueAtTime(150, t); o.frequency.exponentialRampToValueAtTime(40, t + 0.4); break;
             case 'powerup': o.frequency.setValueAtTime(600, t); o.frequency.exponentialRampToValueAtTime(1200, t + 0.05); o.frequency.exponentialRampToValueAtTime(800, t + 0.2); break;
+            // ZMĚNA: Nové zvuky pro štít
+            case 'powerup_shield': o.type = 'sine'; o.frequency.setValueAtTime(800, t); o.frequency.exponentialRampToValueAtTime(1600, t + 0.2); break;
+            case 'shield_break': o.type = 'noise'; g.gain.setValueAtTime(0.3, t); o.frequency.setValueAtTime(1000, t); break;
         }
         g.gain.exponentialRampToValueAtTime(0.001, t + 0.3);
         o.start(t);
@@ -46,4 +49,3 @@ class GameAudio {
 }
 
 export { GameAudio };
-
